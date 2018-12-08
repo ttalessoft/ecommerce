233777e0e33f6dd464cde94ef9269d9bca3210c8
ttalessoft <ttalessoft@gmail.com>
2018-12-08 18:42:39 -0200

    configurando pag-admin e tpl-admin

--- index.php
+++ index.php
@@ -4,15 +4,24 @@
 
     use \Slim\Slim;
     use \Hcode\Page;
+    use \Hcode\PageAdmin;
 
     $app = new Slim();
 
     $app->config('debug', true);
 
+    // Rota da página inicial da loja
     $app->get('/', function(){
         
         $page = new Page();
+        $page->setTpl("index");
 
+    });
+
+    // Rota da página inicial do admin
+    $app->get('/admin', function(){
+
+        $page = new PageAdmin();
         $page->setTpl("index");
 
     });
