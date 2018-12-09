cc0f58c5110659ca0cdb14b24d90b2ad408433d6
ttalessoft <ttalessoft@gmail.com>
2018-12-09 01:44:59 -0200

    Método login e logout

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+    session_start();
+
     require_once("vendor/autoload.php");
 
     use \Slim\Slim;
@@ -54,6 +56,16 @@
 
         // Para a execução dos processos
         exit;
+    });
+
+    $app->get('/admin/logout', function(){
+
+        User::logout();
+
+        header("Location: /admin/login");
+
+        exit;
+
     });
 
     // Método main da aplicação
