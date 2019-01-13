90a9b60219947e33f1f96b2d97d72680722c667f
ttalessoft <ttalessoft@gmail.com>
2019-01-13 13:23:25 -0200

    contas a pagar

--- vendor/hcodebr/php-classes/src/Model/DocPagar.php
+++ vendor/hcodebr/php-classes/src/Model/DocPagar.php
@@ -6,12 +6,29 @@
     use \Hcode\Model;
     use \Hcode\Mailer;
 
+    use \Hcode\Fornecedor;
+    use \Hcode\TipoDoc;
+    use \Hcode\CentroDeCustos;
+    use \Hcode\StatusDoc;
+    use \Hcode\User;
+
     class DocPagar extends Model{
 
         public static function listAll(){
 
             $sql = new Sql();
 
-            return $results = $sql->select("SELECT * FROM tb_doc_pagar");
+            $fornecedor = new Fornecedor();
+            $tipo_doc = new TipoDoc();
+            $centro_de_custos = new CentroDeCustos();
+            $status_doc = new StatusDoc();
+
+            return $results = $sql->select("CALL sp_doc_pagar_save()", array(
+                
+            ));
+
+            $this->setData($results[0]);
+
         }
+
     }
